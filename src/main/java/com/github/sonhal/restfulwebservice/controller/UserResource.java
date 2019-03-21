6b26f7b4db65d8ca16783d9227e9f3d0779ab916
sondre <sondre.hal@gmail.com>
2019-03-21 11:29:24 +0100

    Create UserNotFoundException add give it a HTTP Status

--- src/main/java/com/github/sonhal/restfulwebservice/controller/UserResource.java
+++ src/main/java/com/github/sonhal/restfulwebservice/controller/UserResource.java
@@ -2,10 +2,14 @@ package com.github.sonhal.restfulwebservice.controller;
 
 
 import com.github.sonhal.restfulwebservice.model.User;
+import com.github.sonhal.restfulwebservice.model.UserNotFoundException;
 import com.github.sonhal.restfulwebservice.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,11 +30,21 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return userDaoService.findOne(id);
+        User user = userDaoService.findOne(id);
+        if(user == null) throw new UserNotFoundException("id-" + id);
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
