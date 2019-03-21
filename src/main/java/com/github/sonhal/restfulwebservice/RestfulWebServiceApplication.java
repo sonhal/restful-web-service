032638770f9c6c8f3bf7a3545ef8b8da0ee0b89a
sondre <sondre.hal@gmail.com>
2019-03-21 19:28:29 +0100

    Create internationalization for HelloWorldController. Does not work atm!

--- src/main/java/com/github/sonhal/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/github/sonhal/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,12 +2,32 @@ package com.github.sonhal.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return resourceBundleMessageSource;
 	}
 
 }
--- src/main/java/com/github/sonhal/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/github/sonhal/restfulwebservice/controller/HelloWorldController.java
@@ -2,14 +2,25 @@ package com.github.sonhal.restfulwebservice.controller;
 
 
 import com.github.sonhal.restfulwebservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private final MessageSource messageSource;
 
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/greeting")
     public String greeting(){
@@ -20,4 +31,10 @@ public class HelloWorldController {
     public Greeting greetingBean(@PathVariable String name){
         return new Greeting(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/greeting-internationalized")
+    public String greetingInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message",null, locale);
+    }
+
 }
