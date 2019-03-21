767efdf588c101c81f4676f97de94638b8c038a1
sondre <sondre.hal@gmail.com>
2019-03-21 20:46:42 +0100

    Add Swagger API annotations to describe API models

--- src/main/java/com/github/sonhal/restfulwebservice/model/User.java
+++ src/main/java/com/github/sonhal/restfulwebservice/model/User.java
@@ -1,17 +1,23 @@
 package com.github.sonhal.restfulwebservice.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
 public class User {
 
     private final Integer id;
 
     @Size(min=2, message="Name should have at minimum 2 characters")
+    @ApiModelProperty(notes = "Name should have atleast two characters")
     private  final String name;
 
     @Past
+    @ApiModelProperty(notes = "Birthdate should be before current time")
     private final Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
